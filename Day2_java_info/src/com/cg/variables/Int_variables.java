fdcc42032b4c354929fc2ca9d680c03dc1470026
Anuja <Anuja@DESKTOP-J4EU91V>
2022-07-16 19:14:43 +0530

    first commit

---
+++ Day2_java_info/src/com/cg/variables/Int_variables.java
@@ -0,0 +1,14 @@
+package com.cg.variables;
+
+import java.util.Scanner;
+
+public class Int_variables {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		System.out.println(" the value of a is:"+a );
+		s.close();
+	}
+
+}
