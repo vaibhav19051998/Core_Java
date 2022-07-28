48cd5a7d3551dfba3f5a741c25d4f74a99eaa22f
Anuja <Anuja@DESKTOP-J4EU91V>
2022-07-28 20:40:50 +0530

    first commit

--- Day13_Collection1.1/src/com/cg/arraylist/CloneArrayList.java
+++ Day13_Collection1.1/src/com/cg/arraylist/CloneArrayList.java
@@ -1,9 +1,21 @@
 package com.cg.arraylist;
 
+import java.util.ArrayList;
+
 public class CloneArrayList {
 
 	public static void main(String[] args) {
+		ArrayList<Object> obj=new ArrayList<Object>();
+		obj.add("Shiwani");
+		obj.add(13);
+		obj.add(15.30);
+		obj.add("Shiwani");
+		System.out.println(obj);
 		
+		//Shallow Copy of ArrayList
+		Object clonelist;
+		clonelist=obj.clone();
+		System.out.println(clonelist);
 	}
 
 }
